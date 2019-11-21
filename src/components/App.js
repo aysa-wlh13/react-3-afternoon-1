a03183b8aefe7dedfda5ee800a4667ad963beabe
Aysa Grabert <aysagrabert@Aysas-MacBook-Pro.local>
2019-11-21 11:59:14 -0700

    try that

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,115 @@
+// import React, { Component } from 'react';
+// import axios from 'axios';
+
+// import './App.css';
+
+// import Header from './Header/Header';
+// import Compose from './Compose/Compose';
+// import Post from './Post/Post'
+
+// class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       posts: []
+//     };
+
+//     this.updatePost = this.updatePost.bind( this );
+//     this.deletePost = this.deletePost.bind( this );
+//     this.createPost = this.createPost.bind( this );
+//   }
+  
+//   componentDidMount() {
+//     // axios (GET)
+//     axios.get('https://practiceapi.devmountain.com/api/posts')
+
+//     .then(res => {
+//       // setState with response -> vehiclesToDisplay
+//       this.setState({
+//         posts: res.data
+//       })
+//     })
+//   }
+
+//   updatePost() {
+//     // axios (GET)
+//     axios.put('https://practiceapi.devmountain.com/api/posts?id=${ id }, {text}')
+
+//     .then(res => {
+//       // setState with response -> vehiclesToDisplay
+//       this.setState({
+//         posts: res.data
+
+//       })
+//     })
+//   }
+
+//   deletePost() {
+//     // axios (GET)
+//     axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${ id }`)
+
+//     .then(res => {
+//       // setState with response -> vehiclesToDisplay
+//       this.setState({
+//         posts: res.data
+
+//       })
+//     })
+//   }
+
+//   createPost() {
+//     // axios (GET)
+//     axios.post('https://practiceapi.devmountain.com/api')
+
+//     .then(res => {
+//       // setState with response -> vehiclesToDisplay
+//       this.setState({
+        
+//       })
+//     })
+//   }
+
+//   render() {
+//     const { posts } = this.state;
+  
+//     return (
+//       <div className="App__parent">
+//         <Header />
+  
+//         <section className="App__content">
+  
+//           <Compose />
+          
+//           {
+//             posts.map( post => (
+//               <Post 
+//                 key={ post.id }
+//                 text={post.text}
+//                 date={post.date}
+//                 id={post.id}
+//                 updatePostFn={this.updatePost}
+//                 deletePostFn={ this.deletePost } />
+                
+//             ))
+//           }
+  
+//         </section>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post';
 
 class App extends Component {
   constructor() {
@@ -19,19 +125,51 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    // axios (GET)
+    axios.get('https://practiceapi.devmountain.com/api/posts')
+    
+    .then( res => {
+    // setState 
+      this.setState({ 
+        posts: res.data 
+      });
+    });
   }
 
-  updatePost() {
-  
+  updatePost( id, text ) {
+    // axios (PUT)
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${ id }`, { text })
+
+    .then( res => {
+      // setState 
+      this.setState({ 
+        posts: res.data 
+      });
+    });
   }
 
-  deletePost() {
-
+  deletePost( id ) {
+    // axios (DELETE)
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${ id }`)
+    
+    .then( res => {
+      // setState
+      this.setState({ 
+        posts: res.data 
+      });
+    });
   }
 
-  createPost() {
-
+  createPost( text ) {
+    // axios (POST)
+    axios.post('https://practiceapi.devmountain.com/api/posts', { text })
+    
+    .then( res => {
+      // setState 
+      this.setState({ 
+        posts: res.data 
+      });
+    });
   }
 
   render() {
@@ -43,8 +181,19 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
+          <Compose createPostFn={ this.createPost }/>
           
+          {
+            posts.map( post => (
+              <Post key={ post.id }
+                    id={ post.id }
+                    text={ post.text}
+                    date={ post.date }
+                    updatePostFn={ this.updatePost }
+                    deletePostFn={ this.deletePost } />
+            ))
+          }
+
         </section>
       </div>
     );
--- src/components/Post/Edit/Edit.js
+++ src/components/Post/Edit/Edit.js
@@ -20,11 +20,16 @@ export default class Edit extends Component {
   }
 
   updatePost() {
-
+    const { text } = this.state;
+    const { id, updatePostFn, hideEdit } = this.props;
+  
+    updatePostFn( id, text );
+    hideEdit();
   }
 
   render() {
     // More destructuring!
+    console.log(this.props)
     const { hideEdit } = this.props;
     const { text } = this.state;
 
